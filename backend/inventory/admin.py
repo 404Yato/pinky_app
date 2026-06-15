87998f7acdbd06539353b1e3c8b63dbfb098b630
Cristian Tapia <cristianxd2022@gmail.com>
2026-06-15 10:54:14 -0400

    Add Book Model - Change field restrictions in  Item Model

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Item, ItemType
+from .models import Item, ItemType, Book
 
 admin.site.register(Item)
 admin.site.register(ItemType)
+admin.site.register(Book)
--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ItemType(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -9,7 +12,7 @@ class ItemType(models.Model):
 
 class Item(models.Model):
     user = models.ForeignKey(
-        'auth.User',
+        User,
         on_delete=models.CASCADE
     )
 
@@ -20,10 +23,31 @@ class Item(models.Model):
 
     name = models.CharField(max_length=255)
 
-    description = models.CharField(max_length=255)
+    description = models.CharField(blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     updated_at = models.DateTimeField(auto_now=True)
 
-    deleted_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    item = models.OneToOneField(
+        Item,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+
+    isbn = models.CharField(max_length=20, null=True, blank=True)
+
+    author = models.CharField(null=True, blank=True)
+
+    publisher = models.CharField(null=True, blank=True)
+
+    pages = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
